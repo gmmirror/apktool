a25cce59f88d60f9f84304b3796f25fdab4676ea
gmmirror <blackstar76776@gmail.com>
2026-04-02 18:08:44 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ -d "${PREFIX}/opt/apktool" ]]; then
+    command rm -rf "${PREFIX}/opt/apktool"
+fi
+
+command mkdir -p "${PREFIX}/opt/apktool"
+
+command wget \
+    'https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool' \
+    -O "${PREFIX}/opt/apktool/apktool"
+
+command wget \
+    'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.10.0.jar' \
+    -O "${PREFIX}/opt/apktool/apktool.jar"
+
+command chmod +x "${PREFIX}/opt/apktool/apktool"
+command chmod +x "${PREFIX}/opt/apktool/apktool.jar"
+
+command ln -sf \
+    "${PREFIX}/opt/apktool/apktool" \
+    "${PREFIX}/bin/apktool"
+
+command sed -i \
+    "s|libdir=.*|libdir=\"${PREFIX}/opt/apktool\"|g" \
+    "${PREFIX}/opt/apktool/apktool"
